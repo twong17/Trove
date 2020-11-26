c4b010de1eb5b79c9910400c9c17258afb47b54f
twong17 <70173433+twong17@users.noreply.github.com>
2020-11-26 12:05:26 -0500

    Update userinfo.js

--- models/userinfo.js
+++ models/userinfo.js
@@ -6,9 +6,29 @@ var UserInfoSchema = new Schema(
   {
     username: {type: String, required: true},
     password: {type: String, required: true},
+    firstname: {type: String, required: false},
+    lastname: {type: String, required: false},
+    friends: [{type: Schema.Types.ObjectId, ref: 'UserInfo'}],
   }
 );
 
+UserInfoSchema.set('toJSON', { virtuals: true })
+//// Virtual for user login
+//UserInfoSchema
+//.virtual('url')
+//.get(function () {
+//  return '/catalog/login/' + this._id;
+//});
 
+// Virtual for user's full name
+UserInfoSchema
+.virtual('name')
+.get(function () {
+  return this.firstname + ' ' + this.lastname;
+});
+
+
+
+//const UserInfo = mongoose.model('UserInfo', UserInfoSchema, 'userinfos');
 //Export model
 module.exports = mongoose.model('UserInfo', UserInfoSchema);
